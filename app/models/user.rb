2840ddbe6913d4d47ed9d5f3befbef47b141903c
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-06-13 01:11:58 +0900

    save point

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :trackable,
+         :omniauthable
   include DeviseTokenAuth::Concerns::User
 end
+
+         
