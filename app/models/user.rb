0bdb45642606bc52b250669a3b4fed4059ab8942
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-06-18 00:06:34 +0900

    やっと動いたセットアップ完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-<<<<<<< HEAD
   devise :database_authenticatable,
          :registerable,
          :recoverable,
@@ -14,10 +11,4 @@ class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
 end
 
-         
-=======
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  include DeviseTokenAuth::Concerns::User
-end
->>>>>>> d815b2e299736ab63bfd0f7340ae9a6a5ce3bbc4
+        
