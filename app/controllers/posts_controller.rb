c282788a4e20c16cab2e44b0f6811b49099afda1
rr-rr-re <rr.anonymous.2020@gmail.com>
2021-06-27 14:57:41 +0900

    save before scaffold

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,17 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :update, :destroy]
     def index
         @posts = Post.all
         render json: @posts
     end
+
+    def show
+        render json: @posts
+    end
+    
+    private
+        def set_post
+            @post = Post.fing(params[:id])
+        end
 end
  
